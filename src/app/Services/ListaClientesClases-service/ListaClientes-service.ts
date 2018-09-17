0885d85f1e8eedec44830ca2cf26bbd49d5f23d9
daniel <agerock60@gmail.com>
2018-09-16 20:33:18 -0500

    third commit

--- src/app/Services/ListaClientesClases-service/ListaClientes-service.ts
+++ src/app/Services/ListaClientesClases-service/ListaClientes-service.ts
@@ -3,10 +3,10 @@ import {Cliente} from '../../../app/Classes/Cliente';
 
 const CLIENTES : Array<Cliente>=[
 
-   new Cliente(3,'Marco','Rivera','marco','marco@gmail.com','1SAS455S',52,1.73,75.5,40),
-   new Cliente(4,'Irma','Rivera','irma','irma@gmail.com','123455S',47,1.68,75.5,40),
-   new Cliente(5,'Sofia','Rivera','sophie','sofia@gmail.com','12J45678',5,1.44,20.5,10),
-   new Cliente(6,'Paola','Ortiz','pao','pao@gmail.com','JG5FDE8E',29,1.67,77.0,50),
+   new Cliente(3,'M','ra','co','o@gmail.com','1SAS455S',52,1.73,75.5,40),
+   new Cliente(4,'I','R','a','a@gmail.com','123455S',47,1.68,75.5,40),
+   new Cliente(5,'S','ve','hie','8o@gmail.com','12J45678',5,1.44,20.5,10),
+   new Cliente(6,'P','iz','o','p888@gmail.com','JG5FDE8E',29,1.67,77.0,50),
 ];
 
 const RETRASO = 500;
